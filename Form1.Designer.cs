0f51ddd7624b2699438e4428884905375b518351
Ghostlydestinypolice <roelvanederen@gmail.com>
2021-12-21 14:59:39 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Reversi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NieuwSpel = new System.Windows.Forms.Button();
+            this.HelpKnop = new System.Windows.Forms.Button();
+            this.speelbord = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.NieuwSpel.Location = new System.Drawing.Point(217, 24);
+            this.NieuwSpel.Name = "button1";
+            this.NieuwSpel.Size = new System.Drawing.Size(119, 33);
+            this.NieuwSpel.TabIndex = 0;
+            this.NieuwSpel.Text = "Nieuw Spel";
+            this.NieuwSpel.UseVisualStyleBackColor = true;
+            this.NieuwSpel.Click += new System.EventHandler(this.NieuwSpelKlik);
+            // 
+            // button2
+            // 
+            this.HelpKnop.Location = new System.Drawing.Point(408, 24);
+            this.HelpKnop.Name = "button2";
+            this.HelpKnop.Size = new System.Drawing.Size(119, 33);
+            this.HelpKnop.TabIndex = 1;
+            this.HelpKnop.Text = "Help";
+            this.HelpKnop.UseVisualStyleBackColor = true;
+            this.HelpKnop.Click += new System.EventHandler(this.HelpKlik);
+            // 
+            // speelbord
+            // 
+            this.speelbord.Location = new System.Drawing.Point(80, 80);
+            this.speelbord.Name = "speelbord";
+            this.speelbord.Size = new System.Drawing.Size(600, 600);
+            this.speelbord.TabIndex = 2;
+            this.speelbord.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelTeken);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 744);
+            this.Controls.Add(this.speelbord);
+            this.Controls.Add(this.HelpKnop);
+            this.Controls.Add(this.NieuwSpel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NieuwSpel;
+        private System.Windows.Forms.Button HelpKnop;
+        private System.Windows.Forms.Panel speelbord;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reversi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PanelTeken(object sender, PaintEventArgs pea)
+        {
+            Graphics gr = pea.Graphics;
+            int bordgrootte = 6;
+            int[,] panelarray = new int [6,6];
+            panelarray = new int[bordgrootte, bordgrootte];
+
+            //speelbord begin
+            for (int n = 0; n <= bordgrootte; n++)
+            {
+                gr.DrawLine(Pens.Black, 0 + speelbord.Width / bordgrootte * n, 0, speelbord.Width / bordgrootte * n, speelbord.Height);
+                gr.DrawLine(Pens.Black, 0, speelbord.Height / bordgrootte * n, speelbord.Width, speelbord.Height / bordgrootte * n);
+            }
+        }
+
+        private void HelpKlik(object sender, EventArgs e)
+        {
+            string helptekst = "";
+            MessageBox.Show(helptekst);
+        }
+
+        private void NieuwSpelKlik(object o, EventArgs e)
+        {
+
+        }
+    }
+}
